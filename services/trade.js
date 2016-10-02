822314058c6f6a1ca73f06d83247d824da7fd345
Dylan Lynch <lynchd2@tcnj.edu>
2016-10-02 18:11:26 -0400

    Successfully make sell transactions and cash reflects profit/loss.

--- services/trade.js
+++ services/trade.js
@@ -22,6 +22,7 @@ app.factory('tradeService', ["portfolioService", "transactionService", function(
     if(portfolio["purchased"][symbol] && portfolio["purchased"][symbol][dateIndex]) {
       portfolio["purchased"][symbol][dateIndex]["quantity"] += quantity;
       portfolio.cashOnHand = portfolioService.deductCash((Math.round((quantity * opening) * 100) / 100))
+      //Overwrite old portfolio with new one.
       portfolioService.setPortfolio(portfolio);
     }
     else {
@@ -31,16 +32,44 @@ app.factory('tradeService', ["portfolioService", "transactionService", function(
       portfolio["purchased"][symbol][dateIndex].date = date;
       portfolio["purchased"][symbol][dateIndex].opening = opening;
       portfolio.cashOnHand = portfolioService.deductCash((Math.round((quantity * opening) * 100) / 100))
+      //Overwrite old portfolio with new one.
       portfolioService.setPortfolio(portfolio);
     }
   }
 
   var sellStock = function(formData) {
+    var totalAsked = formData.quantity;
+    var totalOwned = 0
     var portfolio = portfolioService.getPortfolio();
-    console.log(portfolio)
-    // for(var key in portfolio["purchased"][formData.symbol]) {
-    //   console.log(portfolio["purchased"][formData.symbol])
-    // }
+    //Check to see if you have enough stock
+    for(var key in portfolio["purchased"][formData.symbol]) {
+      totalOwned += portfolio["purchased"][formData.symbol][key].quantity
+    }
+    //If you do not own enough stock quantity
+    if(totalAsked > totalOwned) {
+      alert("You do not have " + totalAsked + " shares in " + formData.symbol+ ". You only have " + totalOwned);
+    }
+    //When you do have enough stock
+    else {
+      portfolio.cashOnHand = portfolioService.addCash((Math.round((formData.quantity * formData.opening) * 100) / 100))
+      portfolioService.setPortfolio(portfolio);
+      //Send sell transaction to transaction service to show on transaction page.
+      transactionService.makeTransaction(formData)
+      //Until you sell all you have asked for
+      while(totalAsked > 0) { 
+        for(var key in portfolio["purchased"][formData.symbol]) {
+          //If the day for this purchased stock is greater or equal to what you asked for, sell it off.
+          if(portfolio["purchased"][formData.symbol][key].quantity >= totalAsked) {
+            //Store quantity in temp since it changes
+            var temp = portfolio["purchased"][formData.symbol][key].quantity;
+            console.log(temp);
+            portfolio["purchased"][formData.symbol][key].quantity -= totalAsked;
+            totalAsked -= temp;
+          }
+        }
+      }
+    }
+    portfolioService.setPortfolio(portfolio);
   }
 
   var checkForBought = function(formData) {
